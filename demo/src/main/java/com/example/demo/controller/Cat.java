01494e7c2bf6a04466ad0aeaf29532760ae579ba
khangtv <khang.tranvy@bluegroup.vn>
2024-04-05 16:16:58 +0700

    blassss

---
+++ demo/src/main/java/com/example/demo/controller/Cat.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class Cat {
+    private String name;
+    private String age;
+}
--- demo/src/main/java/com/example/demo/controller/mainController.java
+++ demo/src/main/java/com/example/demo/controller/mainController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +12,22 @@ import java.util.Arrays;
 @RestController
 @RequestMapping("/api/main")
 public class mainController {
+
+    private static final Logger logger = LoggerFactory.getLogger(mainController.class);
+
     @GetMapping()
     public ResponseEntity<?> identification() {
+        Cat cat = new Cat();
+//        cat.setName("name");
+//        cat.setAge("12");
+
+        logger.debug("======>>>>>> hello");
+        logger.trace("doStuff needed more information - {}", cat);
+        logger.debug("doStuff needed to debug - {}", cat);
+        logger.info("doStuff took input - {}", cat);
+        logger.warn("doStuff needed to warn - {}", cat);
+        logger.error("doStuff encountered an error with value - {}", cat);
+        System.out.println("hehe");
         return ResponseEntity.ok("Hello word");
     }
 }
